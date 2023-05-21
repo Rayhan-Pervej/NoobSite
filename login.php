2691e4d08f3a6921bbb67003da2e0592783ef2cf
Rayhan-Pervej <rayhanp1011@gmail.com>
2023-05-21 13:46:30 +0600

    starting

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="noobSite.css">
+    <title>NoobSite Login</title>
+</head>
+<body>
+    <div class="loginPage">
+        <div class="form">
+            <form class="register-form">
+                <input type="text" placeholder="name" />
+                <input type="password" placeholder="password" />
+                <input type="text" placeholder="email address" />
+                <button>create</button>
+                <p class="message">Already registered? <a href="#">Sign In</a></p>
+            </form>
+            <form class="loginForm">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>login</button>
+                <p class="message">Not registered? <a href="#">Create an account</a></p>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.message a').click(function() {
+                $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+            });
+        });
+    </script>
+</body>
+</html>
